67928f731397156c2861ba1b5449f1f63610a89f
vnframe <vnframe95@icloud.com>
2017-06-23 14:49:09 -0400

    My name is

---
+++ names.rb
@@ -0,0 +1,28 @@
+#names.rb
+
+def intro
+	print "My first name is "
+end
+
+def intro2
+	print "My last name is "
+end
+
+def first_name(name)
+	puts name
+end
+
+def last_name(name)
+ 	puts name
+end 
+
+#def introduction
+#	print "My first name is " 
+#	puts first_name("Tori")
+#	puts last_name("Frame")
+#end
+
+intro 
+first_name("Tori")
+intro2
+last_name("Frame")
